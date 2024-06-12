4f87f5d8a9114fd24c8a446a1d7d7afc956a0345
vivekhjtv <vivekhjtv18@gmail.com>
2024-06-12 12:46:46 -0400

    Add rental home with full ui

--- src/components/dashboard/components/rentalHome/index.tsx
+++ src/components/dashboard/components/rentalHome/index.tsx
@@ -1,107 +1,187 @@
 'use client';
-import { Button, Input } from '@nextui-org/react';
+import { Avatar, Button, Input } from '@nextui-org/react';
 import Link from 'next/link';
-import React, { useEffect, useState } from 'react';
-
-// import { AddUser } from './add-user';
 import { HouseIcon } from '../icons/breadcrumb/house-icon';
 import { UsersIcon } from '../icons/breadcrumb/users-icon';
-import { TrashIcon } from '@radix-ui/react-icons';
-import { DotsIcon } from '../icons/accounts/dots-icon';
-import { ExportIcon } from '../icons/accounts/export-icon';
-import { InfoIcon } from '../icons/accounts/info-icon';
-import { SettingsIcon } from '../icons/sidebar/settings-icon';
-import { TableWrapper } from '../table/table';
-import Modal from '@/components/auth/modal';
+// import { TrashIcon } from '@radix-ui/react-icons';
+// import { DotsIcon } from '../icons/accounts/dots-icon';
+// import { ExportIcon } from '../icons/accounts/export-icon';
+// import { InfoIcon } from '../icons/accounts/info-icon';
+// import { SettingsIcon } from '../icons/sidebar/settings-icon';
 
 export const RentalHome = () => {
-  const [modal, setModal] = useState(true);
-
   return (
-    <>
-      <div className="my-14 lg:px-6 max-w-[95rem] mx-auto w-full flex flex-col gap-4">
-        <ul className="flex">
-          <li className="flex gap-2">
-            <HouseIcon />
-            <Link href={'/'}>
-              <span>Home</span>
-            </Link>
-            <span> / </span>{' '}
-          </li>
+    <div className="my-14 lg:px-6 max-w-[95rem] mx-auto w-full flex flex-col gap-4">
+      <ul className="flex">
+        <li className="flex gap-2">
+          <HouseIcon />
+          <Link href={'/'}>
+            <span>Home</span>
+          </Link>
+          <span> / </span>
+        </li>
+        <li className="flex gap-2">
+          <UsersIcon />
+          <span>Users</span>
+          <span> / </span>
+        </li>
+        <li className="flex gap-2">
+          <span>List</span>
+        </li>
+      </ul>
 
-          <li className="flex gap-2">
-            <UsersIcon />
-            <span>Users</span>
-            <span> / </span>{' '}
-          </li>
-          <li className="flex gap-2">
-            <span>List</span>
-          </li>
-        </ul>
-
-        <h3 className="text-xl font-semibold">All Accounts</h3>
-        <div className="flex justify-between flex-wrap gap-4 items-center">
-          <div className="flex items-center gap-3 flex-wrap md:flex-nowrap">
-            <Input
-              classNames={{
-                input: 'w-full',
-                mainWrapper: 'w-full',
-              }}
-              placeholder="Search users"
-            />
-            <SettingsIcon />
-            <TrashIcon />
-            <InfoIcon />
-            <DotsIcon />
-          </div>
-          <div className="flex flex-row gap-3.5 flex-wrap">
-            <Button color="primary" startContent={<ExportIcon />}>
-              Export to CSV
-            </Button>
-          </div>
+      <h3 className="text-xl font-semibold">Rental Home</h3>
+      {/* <div className="flex justify-between flex-wrap gap-4 items-center">
+        <div className="flex items-center gap-3 flex-wrap md:flex-nowrap">
+          <Input
+            classNames={{
+              input: 'w-full',
+              mainWrapper: 'w-full',
+            }}
+            placeholder="Search users"
+          />
+          <SettingsIcon />
+          <TrashIcon />
+          <InfoIcon />
+          <DotsIcon />
         </div>
-        <div className="max-w-[95rem] mx-auto w-full bg-gray-100 rounded-lg">
-          <div className="container mx-auto">
-            <div className="flex justify-between items-center py-4 px-6">
-              <h1 className="text-xl font-bold">Rental Home</h1>
+        <div className="flex flex-row gap-3.5 flex-wrap">
+          <Button
+            color="primary"
+            startContent={<ExportIcon />}
+            className="px-4 py-2 rounded-lg bg-blue-600 text-white hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 transition duration-300"
+          >
+            Export to CSV
+          </Button>
+        </div>
+      </div> */}
+
+      <div className="max-w-[95rem] mx-auto w-full bg-gray-100 dark:bg-gray-800 rounded-lg">
+        <div className="container mx-auto mb-7">
+          <div className="flex justify-between items-center py-4 px-6">
+            <h1 className="text-xl font-bold dark:text-gray-300 pt-6">
+              My Account
+            </h1>
+          </div>
+          <div className="flex flex-wrap px-6 py-1">
+            <div className="w-full md:w-1/2 lg:w-1/3 px-2 py-2">
+              <p className="font-bold text-gray-700 dark:text-gray-300">
+                Occupants
+              </p>
+              <p className="text-gray-500 dark:text-gray-400">
+                2 Adults & Children
+              </p>
             </div>
-            <div className="flex flex-wrap px-6 py-4">
-              <div className="w-full md:w-1/2 lg:w-1/3 px-2 py-2">
-                <p className="font-bold text-gray-700">Occupants</p>
-                <p className="text-gray-500">2 Adults & Children</p>
-              </div>
-              <div className="w-full md:w-1/2 lg:w-1/3 px-2 py-2">
-                <p className="font-bold text-gray-700">Pet</p>
-                <p className="text-gray-500">Yes/No</p>
-              </div>
-              <div className="w-full md:w-1/2 lg:w-1/3 px-2 py-2">
-                <p className="font-bold text-gray-700">Vehicle</p>
-                <p className="text-gray-500">Yes/No</p>
-              </div>
-              <div className="w-full md:w-1/2 lg:w-1/3 px-2 py-2">
-                <p className="font-bold text-gray-700">Employment Status</p>
-                <p className="text-gray-500">Full Time</p>
-              </div>
-              <div className="w-full md:w-1/2 lg:w-1/3 px-2 py-2">
-                <p className="font-bold text-gray-700">Job Title</p>
-                <p className="text-gray-500">Carpenter</p>
-              </div>
-              <div className="w-full md:w-1/2 lg:w-1/3 px-2 py-2">
-                <p className="font-bold text-gray-700">Company</p>
-                <p className="text-gray-500">Rogers</p>
-              </div>
-              <div className="w-full md:w-1/2 lg:w-1/3 px-2 py-2">
-                <p className="font-bold text-gray-700">Personal Income</p>
-                <p className="text-gray-500">$43000</p>
-              </div>
-              <div className="w-full md:w-1/2 lg:w-1/3 px-2 py-2">
-                <p className="font-bold text-gray-700">Household Income</p>
-                <p className="text-gray-500">—</p>
-              </div>
+            <div className="w-full md:w-1/2 lg:w-1/3 px-2 py-2">
+              <p className="font-bold text-gray-700 dark:text-gray-300">Pet</p>
+              <p className="text-gray-500 dark:text-gray-400">Yes/No</p>
+            </div>
+            <div className="w-full md:w-1/2 lg:w-1/3 px-2 py-2">
+              <p className="font-bold text-gray-700 dark:text-gray-300">
+                Vehicle
+              </p>
+              <p className="text-gray-500 dark:text-gray-400">Yes/No</p>
+            </div>
+            <div className="w-full md:w-1/2 lg:w-1/3 px-2 py-2">
+              <p className="font-bold text-gray-700 dark:text-gray-300">
+                Employment Status
+              </p>
+              <p className="text-gray-500 dark:text-gray-400">Full Time</p>
+            </div>
+            <div className="w-full md:w-1/2 lg:w-1/3 px-2 py-2">
+              <p className="font-bold text-gray-700 dark:text-gray-300">
+                Job Title
+              </p>
+              <p className="text-gray-500 dark:text-gray-400">Carpenter</p>
+            </div>
+            <div className="w-full md:w-1/2 lg:w-1/3 px-2 py-2">
+              <p className="font-bold text-gray-700 dark:text-gray-300">
+                Company
+              </p>
+              <p className="text-gray-500 dark:text-gray-400">Rogers</p>
+            </div>
+            <div className="w-full md:w-1/2 lg:w-1/3 px-2 py-2">
+              <p className="font-bold text-gray-700 dark:text-gray-300">
+                Personal Income
+              </p>
+              <p className="text-gray-500 dark:text-gray-400">$43000</p>
+            </div>
+            <div className="w-full md:w-1/2 lg:w-1/3 px-2 py-2">
+              <p className="font-bold text-gray-700 dark:text-gray-300">
+                Household Income
+              </p>
+              <p className="text-gray-500 dark:text-gray-400">—</p>
             </div>
           </div>
         </div>
       </div>
-    </>
+      <div className="max-w-[95rem] mx-auto w-full bg-gray-100 dark:bg-gray-800 rounded-lg">
+        <div className="container mx-auto bg-white dark:bg-gray-700 rounded-lg shadow-md  py-8 px-12">
+          <h1 className="text-xl font-bold dark:text-gray-300 pt-2 pb-8 px-2 py-2">
+            Landlord Information
+          </h1>
+          <div className="flex justify-between items-center border-b pb-4 dark:border-gray-600 px-4">
+            <div className="flex items-center">
+              <Avatar
+                className="w-16 h-16 rounded-full mr-4"
+                src="https://i.pravatar.cc/150?u=a042581f4e29026024d"
+              />
+              <div>
+                <h2 className="text-xl font-bold dark:text-gray-300">
+                  Vivek Jethva
+                </h2>
+                <p className="text-gray-500 dark:text-gray-400">Landlord</p>
+              </div>
+            </div>
+            <div className="text-right">
+              <p className="text-gray-700 dark:text-gray-300 font-medium">
+                +1 (444) - 455 - 6969
+              </p>
+              <p className="text-gray-700 dark:text-gray-300">
+                ABCANDXYZ@GMAIL.COM
+              </p>
+            </div>
+          </div>
+
+          <div className="mt-6 px-6 py-2">
+            <h3 className="text-lg font-bold text-gray-700 dark:text-gray-300">
+              PROPERTY DETAILS
+            </h3>
+            <div className="mt-4">
+              <h4 className="font-bold text-gray-700 dark:text-gray-300">
+                PROPERTY ADDRESS
+              </h4>
+              <p className="text-gray-500 dark:text-gray-400">
+                105 ABC STREET, TORONTO, ON A1C 2B3
+              </p>
+            </div>
+            <div className="mt-4">
+              <h4 className="font-bold text-gray-700 dark:text-gray-300">
+                TYPE
+              </h4>
+              <p className="text-gray-500 dark:text-gray-400">
+                CONDO OR APARTMENT
+              </p>
+            </div>
+            <div className="mt-4">
+              <h4 className="font-bold text-gray-700 dark:text-gray-300">
+                AMENITIES
+              </h4>
+              <ul className="list-disc list-inside text-gray-500 dark:text-gray-400">
+                <li>IN-UNIT LAUNDRY.</li>
+                <li>AIR CONDITIONING AND HEATER.</li>
+                <li>STORAGE SPACE OR LARGE CLOSETS.</li>
+                <li>PATIO OR BALCONY SPACE.</li>
+                <li>DISHWASHER.</li>
+                <li>ENERGY-EFFICIENT APPLIANCES.</li>
+                <li>HIGH-SPEED INTERNET ACCESS.</li>
+                <li>LARGE WINDOWS WITH NATURAL LIGHT.</li>
+                <li>GYM</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
